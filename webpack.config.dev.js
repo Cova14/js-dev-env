d1071320fb4f3868899f47f3db50944c58e80ba2
Arturo Perez <aperez@ecx-group.com>
2018-07-08 20:39:24 -0500

    Webpack config fixed

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,9 @@
-import webpack from 'webpack';
 import path from 'path';
 
 export default {
+  debug: true,
   devtool: 'inline-source-map',
+  noInfo: false,
   entry: [
     path.resolve(__dirname, 'src/index')
   ],
@@ -12,16 +13,11 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-        debug: true,
-        noInfo: false,
-      })
-  ],
+  plugins: [],
   module: {
-    rules: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
     ]
   }
 }
